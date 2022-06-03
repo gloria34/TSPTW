f84844ead38a49549fb9acb782c36b1c202a1561
Sofiia <galkina@dlit.dp.ua>
2022-06-03 22:26:30 +0300

    Add geo search

--- app/src/main/java/dnu/fpm/tsptw/data/entity/DataSet.kt
+++ app/src/main/java/dnu/fpm/tsptw/data/entity/DataSet.kt
@@ -1,5 +1,9 @@
 package dnu.fpm.tsptw.data.entity
 
+import android.location.Address
+import android.location.Geocoder
+import androidx.core.os.ConfigurationCompat
+import dnu.fpm.tsptw.App
 import dnu.fpm.tsptw.data.model.Point
 import dnu.fpm.tsptw.utils.DateUtils
 import java.io.Serializable
@@ -14,10 +18,29 @@ class DataSet(
     }
 
     fun getStartAddress(): String {
-        return "Test Address"
+        val geocoder = Geocoder(
+            App.instance,
+            ConfigurationCompat.getLocales(App.instance.resources.configuration).get(0)
+        )
+        val address: List<Address> =
+            geocoder.getFromLocation(points[0].latitude, points[0].longitude, 1)
+        if (address.isNotEmpty() && address[0].getAddressLine(0) != null) {
+            return address[0].getAddressLine(0)
+        }
+        return "${points[0].latitude}; ${points[0].longitude}"
     }
 
     fun getEndAddress(): String {
-        return "Test Address"
+        val lastIndex = points.size - 1
+        val geocoder = Geocoder(
+            App.instance,
+            ConfigurationCompat.getLocales(App.instance.resources.configuration).get(0)
+        )
+        val address: List<Address> =
+            geocoder.getFromLocation(points[lastIndex].latitude, points[lastIndex].longitude, 1)
+        if (address.isNotEmpty() && address[0].getAddressLine(0) != null) {
+            return address[0].getAddressLine(0)
+        }
+        return "${points[lastIndex].latitude}; ${points[lastIndex].longitude}"
     }
 }
